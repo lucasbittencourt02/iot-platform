31e93416c0ea3d8dc147ea6ae76b027af1ff9661
lucasbittencourt02 <44241183+lucasbittencourt02@users.noreply.github.com>
2021-09-30 09:31:37 -0300

    Criado setup.sh
    setup.sh arquivo de instalação da plataforma

---
+++ setup.sh
@@ -0,0 +1,22 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+sudo apt-get install  curl apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update -y
+sudo apt install docker-ce -y
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+git clone https://github.com/lucasbittencourt02/iot-platform.git
+
+sudo docker start
+
+cd iot-platform/
+sudo docker build ./docker/subscribe/ -t subscribe-mqtt:latest
+
+cd docker/
+sudo docker-compose up -d
